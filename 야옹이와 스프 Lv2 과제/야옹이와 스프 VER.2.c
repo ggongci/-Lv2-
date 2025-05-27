31c3fe990d640ba90faf2c906cf2c8246d9eba45
eunwoooo <tabcun88@naver.com>
2025-05-27 23:34:21 +0900

    2-3 이동 까지 진행

--- 야옹이와 스프 Lv2 과제/야옹이와 스프 VER.2.c
+++ 야옹이와 스프 Lv2 과제/야옹이와 스프 VER.2.c
@@ -16,6 +16,13 @@ int main(void) {
 	int intimacy = 2; //친밀도 2로 시작
 	int feel = 3; //기분 3으로 시작
 	int cp = 0; //cp(cute point). 초기값 0, 매 턴 (MAX(0, 기분-1) + 친밀도) 만큼의 CP를 얻음
+	int interaction = -1;  //상호작용 선택의 값
+	int dice = 0;
+	int zzontteok_pos = HME_POS;         // 현재 쫀떡이 위치
+	int zzontteok_prev_pos = -1;         // 직전 위치 
+	int scratcher_pos = -1;           //스크래처 위치
+	int tower_pos = -1;               //캣 타워 위치          [게임 시작 시 설치되지 않은 상태이므로 -1 초기화를 하는 것!
+	int scratcher = 0, tower = 0; //해당 놀이기구를 샀는지 여부 (0 or 1)
 
 	printf("당신이 지은 야옹이 이름을 다시 한 번 입력하세요: ");  //%c는 문자열을 하나만 다룰 때 사용하고
 	scanf_s("%s", name, 100);                 //%s 는 여러 글자 문자열을 입출력 할 때 사용.  계속해서 한자로 오류난 이유가 %c로 돼 있어서 그랬음 
@@ -50,5 +57,68 @@ int main(void) {
 		printf("==================================================\n\n");
 
 
-		//방 그리기를 기분 나빠짐 코드보다 먼저 써야할지.. 아니면 기분 나빠짐 코드가 우선인지.. 문제로다.
-}
+		//상태출력 이후에는 -> 기분 나빠짐.
+		//기분 나빠짐
+		dice = rand() % 6 + 1;
+		printf("6 - %d: 주사위의 눈이 6-%d이하이면 그냥 기분이 나빠집니다.\n", intimacy, intimacy);  //이 부분은 새로운 변수를 하나 만들 예정!
+		printf("주사위를 굴립니다. 또르르...\n%d이(가) 나왔습니다.\n", dice);
+		if (dice <= (6 - intimacy)) {
+			if (feel > 0) {
+				feel--;
+				printf("%s의 기분이 나빠집니다: %d -> %d\n", name, feel + 1, feel);
+			}
+			else {
+				printf("%s의 기분은 이미 최악입니다.\n", name);
+			}
+		}
+		else {
+			printf("다행히 기분이 유지됩니다.\n");
+		}
+
+		Sleep(1000);
+	}   //현재 코드까지 실행했을 때는 기분 나빠짐 부분만 반복해서 나오는 중 - 수정이 필요할듯
+
+	//이동 & 행동 
+	//기분이 0 일 때 집쪽으로이동
+	if (feel == 0) {
+		if (zzontteok_pos != HME_POS) {
+			printf("기분이 매우 나쁜 %s은(는) 집으로 향합니다.\n", name);
+			zzontteok_pos = HME_POS;    //이 코드가 집으로 이동하게 하는 코드!
+		}
+		else {
+			printf("%s은(는) 집에서 쉼니다.\n", name);
+			if (feel < 3) {
+				feel++;         //기분이 0이면 무조건 집으로 감. 도착 후엔 기분+1 회복!
+				printf("기분이 조금 나아졌습니다: %d -> %d\n", feel - 1, feel);
+			}
+		}
+	}
+	//기분이 1 일 때 더 가까운 놀이기구 쪽으로 이동
+	else if (feel == 1) {
+		if (scratcher && zzontteok_pos != scratcher_pos) {
+			printf("%s은(는) 심심해서 스크래처 쪽으로 이동합니다.\n", name);
+			zzontteok_pos = scratcher_pos;          //이 코드가 스크래처로 이동하게 하는 코드!
+		}
+		else if (tower && zzontteok_pos != tower_pos) {
+			printf("%s은(는) 심심해서 캣타워 쪽으로 이동합니다\n", name);
+			zzontteok_pos = tower_pos;             //이 코드가 타워로 이동하게 하는 코드!
+		}
+		else {
+			printf("놀 거리가 없어서 %s의 기분이 매우 나빠집니다.\n", name);
+			if (feel > 0) feel--;                //스크래처 또는 캣타워가 있으면 그 쪽으로 이동. 없으면 기분-1!
+		}
+	}
+	//기분이 2 일 때 제자리에 대기
+	else if (feel == 2) {
+		printf("%s은(는) 기분 좋게 식빵을 굽고 있습니다.\n", name);
+		// 이동하지 않고 대기 상태. 텍스트만 출력되고 이동 없음
+	}
+	//기분 3 일 때 냄비쪽으로 이동
+	else if (feel == 3) {
+		if (zzontteok_pos != BWL_POS) {
+			printf("%s은(는) 골골송을 부르며 수프를 만들러 갑니다.\n", name);
+			zzontteok_pos = BWL_POS;            //이 코드가 냄비로 이동하게 하는 코드!
+			//냄비 쪽으로 무조건 이동 → 도착 시 수프 만들기 가능
+		}
+	}
+
