59867de71a8885d44662ace3dcb232824faf0ffa
eunwoooo <tabcun88@naver.com>
2025-05-28 21:55:26 +0900

    2-4 행동까지 진행

--- 야옹이와 스프 Lv2 과제/야옹이와 스프 VER.2.c
+++ 야옹이와 스프 Lv2 과제/야옹이와 스프 VER.2.c
@@ -23,6 +23,8 @@ int main(void) {
 	int scratcher_pos = -1;           //스크래처 위치
 	int tower_pos = -1;               //캣 타워 위치          [게임 시작 시 설치되지 않은 상태이므로 -1 초기화를 하는 것!
 	int scratcher = 0, tower = 0; //해당 놀이기구를 샀는지 여부 (0 or 1)
+	int kicked_off_shoes = 0; //집에 막 도착했는지 표시해주는 코드 (0: 도착 X, 1: 이번 턴에 막 도착)
+	//상태를 표현하기 위한 목적은“플래그 변수" 라고 부른답니다.. 
 
 	printf("당신이 지은 야옹이 이름을 다시 한 번 입력하세요: ");  //%c는 문자열을 하나만 다룰 때 사용하고
 	scanf_s("%s", name, 100);                 //%s 는 여러 글자 문자열을 입출력 할 때 사용.  계속해서 한자로 오류난 이유가 %c로 돼 있어서 그랬음 
@@ -76,49 +78,103 @@ int main(void) {
 		}
 
 		Sleep(1000);
-	}   //현재 코드까지 실행했을 때는 기분 나빠짐 부분만 반복해서 나오는 중 - 수정이 필요할듯
+		//현재 코드까지 실행했을 때는 기분 나빠짐 부분만 반복해서 나오는 중 - 수정이 필요할듯
 
-	//이동 & 행동 
-	//기분이 0 일 때 집쪽으로이동
-	if (feel == 0) {
-		if (zzontteok_pos != HME_POS) {
-			printf("기분이 매우 나쁜 %s은(는) 집으로 향합니다.\n", name);
-			zzontteok_pos = HME_POS;    //이 코드가 집으로 이동하게 하는 코드!
-		}
-		else {
-			printf("%s은(는) 집에서 쉼니다.\n", name);
-			if (feel < 3) {
-				feel++;         //기분이 0이면 무조건 집으로 감. 도착 후엔 기분+1 회복!
-				printf("기분이 조금 나아졌습니다: %d -> %d\n", feel - 1, feel);
+		//이동 & 행동 
+		// 이동
+		//기분이 0 일 때 집쪽으로이동
+		zzontteok_prev_pos = zzontteok_pos;
+		if (feel == 0) {
+			if (zzontteok_pos != HME_POS) {
+				printf("기분이 매우 나쁜 %s은(는) 집으로 향합니다.\n", name);
+				zzontteok_pos = HME_POS;    //이 코드가 집으로 이동하게 하는 코드!
+				kicked_off_shoes = 1;
+			}  //현재 위 코드는 집에 막 도착한 턴에도 바로 기분 + 1 을 하는 문제점이 있음. //! 플래그 ! 변수 하나를 추가해서 해결했음
+			else {
+				printf("%s은(는) 집에서 쉼니다.\n", name);
+				if (kicked_off_shoes == 1) {
+					printf("%s은(는) 이번 턴에 막 집에 도착했습니다. 아직 쉬지 않습니다.\n", name);
+					kicked_off_shoes = 0; // 다음 턴부터는 쉴 수 있도록 플래그 변수 초기화
+				}
+				else if (feel < 3) {
+					printf("기분이 조금 나아졌습니다: %d -> %d\n", feel, feel + 1);
+					feel++;         //기분이 0이면 무조건 집으로 감. 도착 후엔 기분+1 회복!
+				}
 			}
 		}
-	}
-	//기분이 1 일 때 더 가까운 놀이기구 쪽으로 이동
-	else if (feel == 1) {
-		if (scratcher && zzontteok_pos != scratcher_pos) {
-			printf("%s은(는) 심심해서 스크래처 쪽으로 이동합니다.\n", name);
-			zzontteok_pos = scratcher_pos;          //이 코드가 스크래처로 이동하게 하는 코드!
+		
+		//기분이 1 일 때 더 가까운 놀이기구 쪽으로 이동
+		else if (feel == 1) {
+			if (scratcher && zzontteok_pos != scratcher_pos) {
+				printf("%s은(는) 심심해서 스크래처 쪽으로 이동합니다.\n", name);
+				zzontteok_pos = scratcher_pos;          //이 코드가 스크래처로 이동하게 하는 코드!
+			}
+			else if (tower && zzontteok_pos != tower_pos) {
+				printf("%s은(는) 심심해서 캣타워 쪽으로 이동합니다\n", name);
+				zzontteok_pos = tower_pos;             //이 코드가 타워로 이동하게 하는 코드!
+			}
+			else {
+				printf("놀 거리가 없어서 %s의 기분이 매우 나빠집니다.\n", name);
+				if (feel > 0) feel--;                //스크래처 또는 캣타워가 있으면 그 쪽으로 이동. 없으면 기분-1!
+			}
 		}
-		else if (tower && zzontteok_pos != tower_pos) {
-			printf("%s은(는) 심심해서 캣타워 쪽으로 이동합니다\n", name);
-			zzontteok_pos = tower_pos;             //이 코드가 타워로 이동하게 하는 코드!
+		//기분이 2 일 때 제자리에 대기
+		else if (feel == 2) {
+			printf("%s은(는) 기분 좋게 식빵을 굽고 있습니다.\n", name);
+			// 이동하지 않고 대기 상태. 텍스트만 출력되고 이동 없음
 		}
-		else {
-			printf("놀 거리가 없어서 %s의 기분이 매우 나빠집니다.\n", name);
-			if (feel > 0) feel--;                //스크래처 또는 캣타워가 있으면 그 쪽으로 이동. 없으면 기분-1!
+		//기분 3 일 때 냄비쪽으로 이동
+		else if (feel == 3) {
+			if (zzontteok_pos != BWL_POS) {
+				printf("%s은(는) 골골송을 부르며 수프를 만들러 갑니다.\n", name);
+				zzontteok_pos = BWL_POS;            //이 코드가 냄비로 이동하게 하는 코드!
+				//냄비 쪽으로 무조건 이동 → 도착 시 수프 만들기 가능
+			}
 		}
-	}
-	//기분이 2 일 때 제자리에 대기
-	else if (feel == 2) {
-		printf("%s은(는) 기분 좋게 식빵을 굽고 있습니다.\n", name);
-		// 이동하지 않고 대기 상태. 텍스트만 출력되고 이동 없음
-	}
-	//기분 3 일 때 냄비쪽으로 이동
-	else if (feel == 3) {
-		if (zzontteok_pos != BWL_POS) {
-			printf("%s은(는) 골골송을 부르며 수프를 만들러 갑니다.\n", name);
-			zzontteok_pos = BWL_POS;            //이 코드가 냄비로 이동하게 하는 코드!
-			//냄비 쪽으로 무조건 이동 → 도착 시 수프 만들기 가능
-		}
-	}
 
+		//행동
+		//아래 코드는 야옹이 Lv1에서 사용한 코드
+		//수프 만드는 코드
+		if (zzontteok_pos == BWL_POS) {
+			int soup_type = rand() % 3;
+			if (soup_type == 0) {
+				printf("%s이(가) 감자 수프를 만들었습니다!\n", name);
+			}
+			else if (soup_type == 1) {
+				printf("%s이(가) 양송이 수프를 만들었습니다!\n", name);
+			}
+			else {
+				printf("%s이(가) 브로콜리 수프를 만들었습니다!\n", name);
+
+			}
+			madesoup++;          //씀으로써 몇 개를 만들었는지 확인할 수있게 해주는 코드.
+		}
+		//위 코드를 배열로 간단히 변경할 수도 있음! 하지만 스스로 아래와 같은 배열을 사용하기엔 부족함을 느낌.
+		/*	if (zzontteok_pos == BWL_POS) {
+				int soup_type = rand() % 3;
+				const char* soup_names[] = { "감자 수프", "양송이 수프", "브로콜리 수프" };
+				printf("%s이(가) %s를 만들었습니다!\n", name, soup_names[soup_type]);
+				madesoup++;
+			} */
+			//스크래처 긁기 코드 O 
+		else if (zzontteok_pos == scratcher_pos && scratcher) {
+			if (feel < 3) {
+				printf("%s은(는) 스크래처를 긁고 놀았습니다.\n", name);
+				printf("기분이 조금 좋아졌습니다: %d -> %d\n", feel, feel + 1);
+				feel++;           //집에서 한 턴을 쉴 때마다 기분 + 1
+			}
+		}
+		else if (zzontteok_pos == tower_pos && tower) {
+			if (feel < 3) {
+				int before = feel;
+				feel += 2;
+				if (feel > 3) feel = 3;
+				printf("%s은(는) 캣타워를 뛰어다닙니다.\n", name);
+				printf("기분이 제법 좋아졌습니다: %d -> %d\n", before, feel);
+			}
+		}
+		Sleep(1500);
+		system("cls");
+	}
+	return 0;
+}
